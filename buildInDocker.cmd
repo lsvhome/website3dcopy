4dcde615b91371025306e1c97619cdf8e7d3fc28
Sergii Lutai <sergii.lutai.ext@dieboldnixdorf.com>
2025-06-29 21:49:20 +0300

    move css into separate folder

--- buildInDocker.cmd
+++ buildInDocker.cmd
@@ -3,4 +3,5 @@ set JEKYLL_VERSION=4.2.0
 :docker run --rm --volume="%CD%:/srv/jekyll" -it jekyll/jekyll sh -c "jekyll build"
 :docker run --publish 4000:4000  --rm --volume="%CD%:/srv/jekyll" -it jekyll/jekyll:%JEKYLL_VERSION% sh -c "jekyll serve"
 :docker run --publish 4000:4000  --rm --volume="%CD%:/srv/jekyll" -it jekyll/jekyll:%JEKYLL_VERSION% sh -c "jekyll serve"
+start http://localhost:4000
 docker run --publish 4000:4000  --rm --volume="%CD%:/srv/jekyll" -it jekyll/jekyll:%JEKYLL_VERSION% sh -c "jekyll serve --force_polling --livereload"
